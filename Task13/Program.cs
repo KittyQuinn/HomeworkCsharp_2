7480b9fd21023058af0cd3fab57a524070a1a1da
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-09-08 18:53:51 +0200

    Added 13 task

---
+++ Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+Console.WriteLine("Write number ");
+int num = int.Parse(Console.ReadLine());
+
+string str = num.ToString();
+
+if (str.Length > 2)
+Console.WriteLine(str[2]);
+else
+{
+    Console.WriteLine("Write another number");
+}
