7e330a448215a22918f19fc0a9c03b547e230e87
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-09-08 17:12:14 +0200

    Added 10 task

---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.WriteLine("Write three-digital number ");
+int num = int.Parse(Console.ReadLine());
+
+int a = num % 100;
+int b = num % 10;
+int c = (a-b)/10;
+
+Console.WriteLine(c);
