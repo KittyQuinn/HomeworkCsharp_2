be49d804bc12ac7aeeec4b7ba1e878e974774767
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-09-08 19:01:05 +0200

    Added task 15

---
+++ Task15/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+Console.WriteLine("Write number ");
+int num = int.Parse(Console.ReadLine());
+
+if (num == 1 || num == 2 || num == 3 || num == 4 || num == 5)
+{
+    Console.WriteLine("This day is a work day");
+}
+
+else if (num == 6 || num  == 7)
+{
+    Console.WriteLine("This day is a day-off");
+}
+
+else 
+{
+    Console.WriteLine("Write number from 1 to 7");
+}
